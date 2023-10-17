40286eaa6a3038d8294b050ff8ae47324d361f30
TheBlackBeardPirate <128419785+TheBlackBeardPirate@users.noreply.github.com>
2023-10-17 09:43:41 -0300

    Update __init__.py

--- API/api/__init__.py
+++ API/api/__init__.py
@@ -4,12 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
+from flask_wtf.csrf import CSRFProtect
 import pymysql
 
 pymysql.install_as_MySQLdb()
 
+csrf = CSRFProtect()
+
 db = SQLAlchemy()
 app = Flask(__name__)
+csrf.init_app(app)
 app.config.from_object('config')
 db.init_app(app)
 ma = Marshmallow(app)
